1bbd3f55869b7bd1a4bf7c3d4da9c24996d8d1ef
yimingwang800 <yimingwang800@gmail.com>
2023-06-15 17:54:28 -0400

    updated

--- Fundamentals/InputOutput.py
+++ Fundamentals/InputOutput.py
@@ -8,6 +8,6 @@ y = 7654321.7654321
 # Format method 1
 print("x is {}, y is {}".format(x, y))
 # Format method 2
-print("x is {: >8}, y is {:.2f}".format(x, y))
+print("x is {: >8}, y is {:,.2f}".format(x, y))
 # Format method 3
-print(f"x is {x: >8}, y is {y:.2f}")
+print(f"x is {x: >8}, y is {y:,.2f}")

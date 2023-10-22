6cdb3907f4bc078af7c3b2e4fe0715320b813947
yimingwang800 <yimingwang800@gmail.com>
2023-10-22 19:06:17 -0400

    updated

--- Fundamentals/2.7 Function.py
+++ Fundamentals/2.7 Function.py
@@ -4,7 +4,7 @@ def my_function():
     print("Hello from a function")
 def my_function_1(first_str, second_str):
     print(first_str + " " + second_str)
-def child_function(child_1, child_2, child_3="Midorya"):
+def child_function(child_1, child_2, child_3="Bob"):
     print("The third child is " + child_3)
 def return_function(x):
     return 5*x
@@ -13,8 +13,8 @@ y = return_function(x=3)
 print (y)
 print(return_function(x=5))
 
-child_function(child_2="Bakugo", child_1="Shoto")
-child_function(child_1="Shoto", child_2="Bakugo", child_3="Eri")
+child_function(child_2="Joe", child_1="Fred")
+child_function(child_1="Fred", child_2="Joe", child_3="Tom")
 
 my_function_1("Cool and", "Awesome")
 
